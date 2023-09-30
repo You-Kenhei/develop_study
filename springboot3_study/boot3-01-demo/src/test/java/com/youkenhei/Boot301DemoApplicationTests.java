978a4d9e781200a7a2a0f378c74b25a27e4b2969
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-09-30 16:17:21 +0900

    1

---
+++ springboot3_study/boot3-01-demo/src/test/java/com/youkenhei/Boot301DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youkenhei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot301DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
