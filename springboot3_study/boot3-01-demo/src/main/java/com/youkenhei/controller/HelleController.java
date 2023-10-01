6b412ebad8721cd9ed37865a33e7095044fe8552
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-01 13:14:32 +0900

    2

---
+++ springboot3_study/boot3-01-demo/src/main/java/com/youkenhei/controller/HelleController.java
@@ -0,0 +1,12 @@
+package com.youkenhei.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelleController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello SpringBoot3!";
+    }
+}
