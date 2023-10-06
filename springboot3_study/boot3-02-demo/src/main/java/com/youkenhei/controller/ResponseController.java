693e51d5ac756e913142dbc6c30cbfb463b0f576
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-07 00:46:41 +0900

    1

--- springboot3_study/boot3-02-demo/src/main/java/com/youkenhei/controller/ResponseController.java
+++ springboot3_study/boot3-02-demo/src/main/java/com/youkenhei/controller/ResponseController.java
@@ -1,6 +1,7 @@
 package com.youkenhei.controller;
 
 import com.youkenhei.pojo.Address;
+import com.youkenhei.pojo.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +12,21 @@ import java.util.List;
 public class ResponseController {
 
     @RequestMapping("/getStr")
-    public String getStr() {
+    public Result getStr() {
         System.out.println("getStr");
-        return "getStr done";
+        return Result.success("getStr");
     }
 
     @RequestMapping("/getInstance")
-    public Address getInstance() {
+    public Result getInstance() {
         Address addr = new Address();
         addr.setCity("北京");
         addr.setProvince("海淀");
-        return addr;
+        return Result.success(addr);
     }
 
     @RequestMapping("/getListInstance")
-    public List<Address> getListInstance() {
+    public Result getListInstance() {
         List<Address> addrs = new ArrayList<>();
         Address addr1 = new Address();
         addr1.setCity("北京");
@@ -43,6 +44,6 @@ public class ResponseController {
         addrs.add(addr2);
         addrs.add(addr3);
 
-        return addrs;
+        return Result.success(addrs);
     }
 }
