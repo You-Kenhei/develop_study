0975a9f0078cf26d413a7580ae1596d78877042f
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-15 02:12:50 +0900

    4

--- springboot3_study/springboot-tlias/src/main/java/com/youkenhei/controller/DeptController.java
+++ springboot3_study/springboot-tlias/src/main/java/com/youkenhei/controller/DeptController.java
@@ -10,26 +10,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
+@RequestMapping("/depts")
 @RestController
 public class DeptController {
     @Autowired
     private DeptService deptService;
 
-    @GetMapping("/depts")
+    @GetMapping
     public Result list() {
         log.info("查询全部部门数据");
         List<Dept> data = deptService.list();
         return Result.success(data);
     }
 
-    @DeleteMapping("/depts/{id}")
+    @DeleteMapping("/{id}")
     public Result delete(@PathVariable Integer id) {
         log.info("根据id删除部门:{}", id);
         deptService.delete(id);
         return Result.success();
     }
 
-    @PostMapping("/depts")
+    @PostMapping
     public Result add(@RequestBody Dept dept){
         log.info("新增部门：{}",dept);
         deptService.add(dept);
