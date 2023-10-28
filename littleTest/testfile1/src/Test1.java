8be276c0cb389477dbb0843916ded71edbee705b
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-28 23:54:49 +0900

    7

---
+++ littleTest/testfile1/src/Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int index = 0;
+        int[] arr = {0, 1, 2, 3, 4, 5, 6};
+
+//        System.out.println(arr[index]);
+//        System.out.println(arr[index++]);
+//        System.out.println(index);
+        System.out.println(arr.length);
+        System.out.println(arr[6]);
+        System.out.println(arr[0 - 2]);
+    }
+}
