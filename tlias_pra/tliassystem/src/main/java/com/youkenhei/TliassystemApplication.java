396c7d9b606909fb21a710e771337b5bf53e3d7f
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-11-01 09:17:52 +0900

    9

---
+++ tlias_pra/tliassystem/src/main/java/com/youkenhei/TliassystemApplication.java
@@ -0,0 +1,13 @@
+package com.youkenhei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliassystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliassystemApplication.class, args);
+    }
+
+}
---
+++ tlias_pra/tliassystem/src/test/java/com/youkenhei/TliassystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youkenhei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliassystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
