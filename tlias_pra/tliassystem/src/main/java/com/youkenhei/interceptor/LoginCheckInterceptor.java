ff5dd2bd3a7582ed3cc623fc79824bd6c1eae3b8
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-11-06 09:39:39 +0900

    12

---
+++ tlias_pra/tliassystem/src/main/java/com/youkenhei/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,54 @@
+package com.youkenhei.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import com.youkenhei.pojo.Result;
+import com.youkenhei.utils.JWTUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+        String url = req.getRequestURL().toString();
+        if (url.contains("/login")) {
+            return true;
+        }
+
+        String jwt = req.getHeader("token");
+        if (!StringUtils.hasLength(jwt)) {
+            Result error = Result.error("NOT_LOGIN");
+            String result = JSONObject.toJSONString(error);
+            resp.getWriter().write(result);
+            return false;
+        }
+
+
+        try {
+            JWTUtils.parseJwt(jwt);
+        } catch (Exception e) {
+            log.info("JWT令牌解析失败");
+            Result error = Result.error("认证失败，请联系管理员");
+            String result = JSONObject.toJSONString(error);
+            resp.getWriter().write(result);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("拦截后业务");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("渲染后业务");
+    }
+}
