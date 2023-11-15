ca63c6518a7d2633e25f7a395b1525766340dbb2
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-11-15 20:43:12 +0900

    mybatis plus study

---
+++ mybatisplusStudy/src/main/java/com/youkenhei/MybatisplusStudyApplication.java
@@ -0,0 +1,13 @@
+package com.youkenhei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusStudyApplication.class, args);
+    }
+
+}
