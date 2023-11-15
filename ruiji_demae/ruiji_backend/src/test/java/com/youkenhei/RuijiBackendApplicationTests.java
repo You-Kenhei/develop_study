9074dc176cbee27130efb4b08b3035697fdf98ed
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-11-15 09:05:29 +0900

    瑞吉外卖平台
    练习用开发

---
+++ ruiji_demae/ruiji_backend/src/test/java/com/youkenhei/RuijiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youkenhei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuijiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
