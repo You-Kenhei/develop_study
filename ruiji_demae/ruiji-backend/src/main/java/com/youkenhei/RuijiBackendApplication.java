e120b9ceaa7d92e7f685da332a09750712ea5705
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-11-15 09:21:40 +0900

    2.1

--- ruiji_demae/ruiji-backend/src/main/java/com/youkenhei/RuijiBackendApplication.java
+++ ruiji_demae/ruiji-backend/src/main/java/com/youkenhei/RuijiBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RuijiBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RuijiBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RuijiBackendApplication.class, args);
+    }
 
 }
--- ruiji_demae/ruiji-backend/src/test/java/com/youkenhei/RuijiBackendApplicationTests.java
+++ ruiji_demae/ruiji-backend/src/test/java/com/youkenhei/RuijiBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RuijiBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
