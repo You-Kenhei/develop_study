31655ccbca992031051fe650c334d30966ad5820
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-11-19 14:05:02 +0900

    3.3

--- mybatisPlusStudy/src/main/java/com/youkenhei/pojo/User.java
+++ mybatisPlusStudy/src/main/java/com/youkenhei/pojo/User.java
@@ -2,15 +2,18 @@ package com.youkenhei.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
 @TableName("t_user")
 public class User {
-    @TableId(value = "uid",type = IdType.AUTO)
+    @TableId(value = "uid", type = IdType.AUTO)
     private Long id;
     private String name;
     private Integer age;
     private String email;
+    @TableLogic
+    private Integer isDelete;
 }
--- mybatisplusStudy/src/test/java/com/youkenhei/MybatisplusStudyApplicationTests.java
+++ mybatisplusStudy/src/test/java/com/youkenhei/MybatisplusStudyApplicationTests.java
@@ -1,5 +1,6 @@
 package com.youkenhei;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.youkenhei.mapper.UserMapper;
 import com.youkenhei.pojo.User;
 import org.junit.jupiter.api.Test;
@@ -16,9 +17,14 @@ class MybatisPlusStudyApplicationTests {
 
     @Test
     public void test1() {
-        List<User> list = userMapper.selectList(null);
-        list.forEach(System.out::println);
+//        userMapper.deleteById(4L);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", "a").
+                between("age", 20, 30).
+                isNotNull("email");
 
+        List<User> list = userMapper.selectList(queryWrapper);
+        list.forEach(System.out::println);
     }
 
 }
