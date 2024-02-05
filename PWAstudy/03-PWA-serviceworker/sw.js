4cf4f5b06069b7b869e019bd28d84394ac83e010
You-Kenhei <you.kenhei.yjp@outlook.com>
2024-02-05 10:53:17 +0900

    3

--- PWAstudy/03-PWA-serviceworker/sw.js
+++ PWAstudy/03-PWA-serviceworker/sw.js
@@ -1,1 +1,14 @@
-console.log('haha')
+self.addEventListener('install', event => {
+    console.log("安装")
+
+})
+
+self.addEventListener('activate', event => {
+    console.log("激活")
+    
+})
+
+self.addEventListener('fetch', event => {
+    console.log("捕捉")
+    
+})
